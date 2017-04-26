558965858d7ba830fb54bec90d6f544610f70b96
Farhad <farhad.hsni@gmail.com>
2017-04-26 22:37:39 +0430

    Stealing from exercism user *josephan*, oh man

--- sublist/sublist.exs
+++ sublist/sublist.exs
@@ -3,30 +3,19 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, b, status \\ nil)
-
-  def compare(a, b, _) when length(a) == length(b) and a !== b,  do: :unequal
-  def compare(a, b, :sublist) when a === b,  do: :sublist
-  def compare(a, b, _) when a === b,  do: :equal
-  def compare([], _, _), do: :sublist
-  def compare(a, b, _) when length(a) < length(b) do
-    b
-    |> Enum.chunk(length(a))
-    |> Enum.member?(a)
-    |> check(a, b)
-  end
-
-  def compare(a, b, _) when length(a) > length(b) do
-    case compare(b, a) do
-      :sublist -> :superlist
-      _        ->
-        [_whatever | tail_a] = a
-        compare(b, tail_a)
+  def compare(a, b) do
+    cond do
+      a === b                                  -> :equal
+      length(a) < length(b) and sublist?(a, b) -> :sublist
+      length(a) > length(b) and sublist?(b, a) -> :superlist
+      true                                     -> :unequal
     end
   end
 
-  def check(true, _a, _b),  do: :sublist
-  def check(_result, a, [_whatever | tail_b]) do
-    compare(a, tail_b, :sublist)
+  def sublist?([], _b), do: true # empty is a sublist of anything
+  def sublist?(a, b) do
+    b
+    |> Stream.chunk(length(a), 1)
+    |> Enum.member?(a)
   end
 end
