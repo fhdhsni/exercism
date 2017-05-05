809a1e69c607155219d866d74c85a67563e85ae5
Farhad <farhad.hsni@gmail.com>
2017-05-05 20:18:41 +0430

    matrix

--- matrix/matrix.exs
+++ matrix/matrix.exs
@@ -1,8 +1,4 @@
 defmodule Matrix do
-  @doc """
-  Convert an `input` string, with rows separated by newlines and values
-  separated by single spaces, into a `Matrix` struct.
-  """
   @spec from_string(input :: String.t()) :: []
   def from_string(input) do
     input
@@ -11,10 +7,6 @@ defmodule Matrix do
     |> Enum.map(&Enum.map(&1, fn str -> String.to_integer(str) end))
   end
 
-  @doc """
-  Write the `matrix` out as a string, with rows separated by newlines and
-  values separated by single spaces.
-  """
   @spec to_string(matrix :: []) :: String.t()
   def to_string(matrix) do
     matrix
@@ -22,21 +14,12 @@ defmodule Matrix do
     |> Enum.join("\n")
   end
 
-  @doc """
-  Given a `matrix`, return its rows as a list of lists of integers.
-  """
   @spec rows(matrix :: []) :: list(list(integer))
   def rows(matrix), do: matrix
 
-  @doc """
-  Given a `matrix` and `index`, return the row at `index`.
-  """
   @spec row(matrix :: [], index :: integer) :: list(integer)
   def row(matrix, index), do: Enum.at(matrix, index)
 
-  @doc """
-  Given a `matrix`, return its columns as a list of lists of integers.
-  """
   @spec columns(matrix :: [], index :: integer, acc :: list(integer)) :: list(list(integer))
   def columns(matrix, column_index \\ 0, acc \\ [])
   def columns(matrix, column_index, acc) when length(matrix) == column_index, do: Enum.reverse(acc)
@@ -47,9 +30,6 @@ defmodule Matrix do
     columns(matrix, column_index + 1, [column | acc])
   end
 
-  @doc """
-  Given a `matrix` and `index`, return the column at `index`.
-  """
   @spec column(matrix :: [], index :: integer) :: list(integer)
   def column(matrix, index) do
     columns(matrix)
